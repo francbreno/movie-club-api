1ab4eaf3fe392b4d1b77fa8b69d81952273f88ad
Breno Soares <francbreno@gmail.com>
2018-12-02 19:54:46 -0300

    added a knex repo

---
+++ contexts/repo.js
@@ -0,0 +1,28 @@
+// Uses knex to create a Repository for data access
+module.exports = knex => tableName => ({
+  create(data) {
+    return knex(tableName).insert(data);
+  },
+  getById(id) {
+    return knex(tableName).where({ id });
+  },
+  getAll() {
+    return knex(tableName);
+  },
+  update(id, data) {
+    return knex(tableName).where({ id }).update(data);
+  },
+  delete(id) {
+    return knex(tableName).where({ id }).delete();
+  },
+  find(filter) {
+    return knex(tableName).where(filter);
+  },
+  // TODO
+  // preload(table) {
+  //   return knex(tableName).leftJoin(table, `${tableName}.id`, `${table}.id`)
+  // },
+  query() {
+    return knex(tableName);
+  },
+});
