a54f3dea247dda57ac26b14a8113c741cb14422e
Breno Soares <francbreno@gmail.com>
2018-12-04 07:20:14 -0300

    created seed file for users to use in tests

---
+++ migrations/seeds/test/users_seed.js
@@ -0,0 +1,30 @@
+const users = [
+  {
+    name: 'John Milton',
+    username: 'johnmilton',
+    credential: {
+      email: 'milton.john@gmail.com',
+      password: 'p@radi5eL0st',
+    },
+  },
+  {
+    name: 'Bernard Cornwell',
+    username: 'littlebernard',
+    credential: {
+      email: 'cornwell@writters.com',
+      password: 'arthur4Ever',
+    },
+  },
+];
+
+const insertUser = (knex, user) => {
+  const { credential, ...userData } = user;
+  return knex('users').insert(userData).first()
+    .then(insertedUser => knex('credentials').insert({ ...credential, insertedUser_id: insertedUser.id }));
+};
+
+exports.seed = function seed(knex, Promise) {
+  return knex('credentials').del()
+    .then(knex('users'))
+    .then(() => Promise.all(users.map(insertUser)));
+};
