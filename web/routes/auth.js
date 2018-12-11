5835d23f0e621ebea89a17259b5172eecf3e122f
Breno Soares <francbreno@gmail.com>
2018-12-10 22:22:14 -0300

    added route to show current user

--- web/routes/auth.js
+++ web/routes/auth.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const authController = require('../controllers/authController');
 
 router.post('/', authController.newOne);
+router.get('/me', authController.show);
 
 module.exports = router;
