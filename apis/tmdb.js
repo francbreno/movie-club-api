6428c4cdc5a51f25af83ff7c12cbce0d54eb9b3d
Breno <francbreno@gmail.com>
2018-12-19 14:02:33 -0300

    removed tmdb key from comments :man_facepalming:

--- apis/tmdb.js
+++ apis/tmdb.js
@@ -4,11 +4,8 @@ const API_ENDPOINT = process.env.TMDB_API_ENDPOINT;
 const API_IMAGES_ENDPOINT = process.env.TMDB_IMAGES_ENDPOINT;
 const API_KEY = process.env.TMDB_API_KEY;
 
-// https://api.themoviedb.org/3/search/movie?api_key=c09049db1c5f3ae6a49f35f848b05d9b&query=matrix&include_adult=false
 const normalizeSearchURL = title => `${API_ENDPOINT}/search/movie?api_key=${API_KEY}&query=${title}&include_adult=false`;
-// https://api.themoviedb.org/3/movie/603?api_key=c09049db1c5f3ae6a49f35f848b05d9b
 const normalizeMovieDetailsURL = id => `${API_ENDPOINT}/movie/${id}?api_key=${API_KEY}`;
-//
 const normalizeHeaderImageURL = path => `${API_IMAGES_ENDPOINT}/${path}`;
 
 // TODO escape title
